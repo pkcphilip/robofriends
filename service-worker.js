6e2a8a400185a1cd2a6b4813c58b6e0a39bb5cd9
Philip Pang <pkcphilip@outlook.com>
2020-05-24 09:14:03 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.bf35aa50bba96db90c07249ee6e7c281.js"
+  "/robofriends/precache-manifest.3b996a336b014d0a28a36e8261223523.js"
 );
 
 self.addEventListener('message', (event) => {
