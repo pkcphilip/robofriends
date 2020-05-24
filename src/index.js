212a0d45741b274eff410b7e22816eef281c1ec0
Philip Pang <pkcphilip@outlook.com>
2020-05-24 11:43:17 +0800

    Added redux to project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
-import 'tachyons';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider, connect } from "react-redux";
+import { createStore } from "redux";
+import { searchRobots } from "./reducers";
+import App from "./containers/App";
+import registerServiceWorker from "./registerServiceWorker";
+import "tachyons";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(searchRobots);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
